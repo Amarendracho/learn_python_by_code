db7fd3d2de41ed6da6e35db208e944455b1d394b
Amarendra <Kadambala.amarendra@gmail.com>
2026-01-08 22:39:35 -0500

    update labmdafunction file

--- functional_programming/lambdafunctions.py
+++ functional_programming/lambdafunctions.py
@@ -1,14 +1,14 @@
 # syntax ->  lambda variable : expression
 
-# # square number using normal method
-# def square(num):
-#     return num * num
-#
-# print(square(20))
-#
-# #With lambda expression
-# square = lambda num: num * num
-# print(square(10))
+# square number using normal method
+def square(num):
+    return num * num
+
+print(square(20))
+
+#With lambda expression
+square = lambda num: num * num
+print(square(10))
 
 # cube a number 1 parameter
 cube = lambda num: num * num * num
@@ -39,7 +39,7 @@ age_check = lambda age: "vote eligible" if age >= 18 else "not eligible"
 print(age_check(25))
 
 
-# When to use lambda - Real time
+# When to use lambda - Real time  sorted() with key=
 data = [("amar", 29), ("nari", 27), ("justin", 31)]
 sorted_date = sorted(data, key = lambda item: item[1])
 print(sorted_date)
@@ -51,3 +51,22 @@ users = [
     {"name": "justin", "age": 31}
 ]
 print(sorted(users, key = lambda u: u["age"]))
+
+# map()
+nums = [2,4,5,6,8]
+square= list(map(lambda x: x * x, nums))
+print(square)
+
+# filter() (keep only matching items)
+nums = [2,4,5,7,8,10]
+evens = list(filter(lambda n: n % 2 ==0, nums))
+print(evens)
+
+# max() / min() with key
+items = [("a", 3), ("b", 9), ("c", 1)]
+best = max(items, key=lambda x: x[1])
+print(best)
+
+# sorted()
+words = ["apple", "kiwi", "banana","pineapple","ice"]
+print(sorted(words, key = lambda x: len(x)))
