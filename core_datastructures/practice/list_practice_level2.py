2e224d211c8c0160847b23a46cad3fb5cdfb1ce0
Amarendra <Kadambala.amarendra@gmail.com>
2026-01-16 20:03:53 -0500

    add new file name practice leve2

--- core_datastructures/practice/list_practice_level2.py
+++ core_datastructures/practice/list_practice_level2.py
@@ -15,7 +15,23 @@
                                 ###################SOLUTIONS################
 
 # 1️⃣1️⃣ Find the sum of all elements in a list.
+nums = [10,20,30,40,50]
+sum = 0
+
+for num in nums:
+    sum += num
+
+print(sum)
+
 # 1️⃣2️⃣ Find the maximum and minimum value in a list (no max() / min()).
+nums = [10,200,30,40,50]
+max_value = nums[0]
+for i in nums:
+    if i > max_value:
+        max_value = i
+
+print(max_value)
+
 # 1️⃣3️⃣ Count how many even numbers are in a list.
 # 1️⃣4️⃣ Count how many odd numbers are in a list.
 # 1️⃣5️⃣ Create a new list containing only numbers greater than 50.
