52938d9186e064f609085d97646bd3fd98da8458
Amarendra <Kadambala.amarendra@gmail.com>
2026-01-20 17:12:19 -0500

    update set.py file

--- core_datastructures/set.py
+++ core_datastructures/set.py
@@ -1,20 +1,28 @@
 
 # collection = single "variable" stored multiple values.
-# Set = {} un-ordered and immutable (not changeable). Add/Remove OK. No Duplicates allowed.
+# Set = {} un-ordered and mutable (can changeable). Add/Remove OK. No Duplicates allowed.
 
-fruits = {"apple", "jackfruit", "banana", "cherry"}
-print(dir(fruits)) # set provide different attributes and methods
-print(help(fruits))
+#fruits = {"apple", "jackfruit", "banana", "cherry"}
+# print(dir(fruits)) # set provide different attributes and methods
+# print(help(fruits))
 
 #'add', 'clear', 'copy', 'difference', 'difference_update', 'discard', 'intersection', 'intersection_update', 'isdisjoint',
 # 'issubset', 'issuperset', 'pop', 'remove', 'symmetric_difference', 'symmetric_difference_update', 'union', 'update'
 
-fruits.add("pineapple") # adding an element
-fruits.remove("apple") # removing apple
-fruits.pop() # remove the random element because set doesn't follow the order
-fruits.clear()
-print(fruits)
+# fruits.add("pineapple") # adding an element
+# print(fruits)
+# fruits.remove("apple") # removing apple
+# fruits.pop() # remove the random element because set doesn't follow the order
+# #fruits.clear()
+# print(fruits)
+#
+# #Iteration
+# for fruit in fruits:
+#     print(fruit)
 
-#Iteration
-for fruit in fruits:
-    print(fruit)
+
+
+
+"""
+        SET - UN-ORDERED , NO-DUPLICATES , IMMUTABLE
+"""
