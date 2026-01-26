3bf9911bfb09196f36b1b19604f9791b156fa714
Amarendra <Kadambala.amarendra@gmail.com>
2026-01-25 20:08:09 -0500

    update set.py file

--- core_datastructures/practice/test.py
+++ core_datastructures/practice/test.py
@@ -1,19 +1,3 @@
 name = "amarendra              "
 
 print(name[::-1])
-
-name = name.strip() #
-print(name)
-
-name = name + "kadambala"
-print(name)
-
-
-ascii_numbers = 0
-
-for char in name:
-    print(char, end=" ")
-    #ascii_numbers += int(char)
-
-print(ascii_numbers)
-
