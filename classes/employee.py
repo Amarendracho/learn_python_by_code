7bf19d264ae8e437efd523954775fb6f04833fb8
Amarendra <Kadambala.amarendra@gmail.com>
2026-01-30 11:38:57 -0500

    added class directory add test file

--- classes/employee.py
+++ classes/employee.py
@@ -1,5 +1,6 @@
 
-"""THIS IS CLASS AND CREATE CLASS VARIABLE
+"""TO CREATE A CLASS KEYWORD IS class classname,
+   THIS IS CLASS WITH CLASS VARIABLE
    FETCHING CLASS VARIABLE USING CLASSNAME.VARIABLE NAME"""
 
 class Employee:
@@ -7,3 +8,4 @@ class Employee:
     name = "Amarendra"
 
 print(Employee.name)
+
