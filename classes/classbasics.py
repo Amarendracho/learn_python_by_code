9e3369fb84dd19e77d3a6f0c7e4766669206a4ce
Amarendra <Kadambala.amarendra@gmail.com>
2026-01-30 12:12:31 -0500

    adding README.cd file

--- classes/classbasics.py
+++ classes/classbasics.py
@@ -2,10 +2,10 @@
 """ A CLASS IS COLLECTION OF STATE(variables) and BEHAVIOR(methods)
     TO CREATE A CLASS KEYWORD IS class classname,
    THIS IS CLASS WITH CLASS VARIABLE
-   FETCHING CLASS VARIABLE USING CLASSNAME.VARIABLE NAME"""
+   FETCHING CLASS VARIABLE USING CLASSNAME.VARIABLE NAME
+   CLASS HAVE CLASS VARIABLES INSTANCE VARIABLES (variables inside constructor) __init__ method"""
 
 class Employee:
-
     name = "Amarendra"
 
 print(Employee.name)
