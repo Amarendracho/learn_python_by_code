bb91620cd9e18dc73d81999c7622ebb4eaa8f3b9
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-02 17:54:31 -0500

    update constructor file

--- oops/object/constructor.py
+++ oops/object/constructor.py
@@ -4,31 +4,36 @@
 
 
 #__init__ with Parameters
-class Human:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-person1 = Human("patric", 19)
-person2 = Human("Justin", 24)
-
-print(person1.name , person1.age)
-print(person2.name , person2.age)
+# class Human:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+# person1 = Human("patric", 19)
+# person2 = Human("Justin", 24)
+#
+# print(person1.name , person1.age)
+# print(person2.name , person2.age)
 
 
 #Default Parameters in __init__
 class OpenAiModels:
 
-    def __init__(self, model_name, age=2, usage = "AI"):
+    def __init__(self, model_name, usage = "AI" , age=2):
         self.model_name = model_name
-        self.age = age
         self.usage = usage
+        self.age = age
 
-model1 = OpenAiModels("chatgpt")
-model2 = OpenAiModels("gemini", 3, "google AI")
+
+model1 = OpenAiModels("chatgpt", "openai")
+model2 = OpenAiModels("gemini", "google AI", 3 )
+model3 = OpenAiModels("claude", "claude ai",1, )
+model4 = OpenAiModels("grook","twitter")
 
 print(model1.model_name, model1.age, model1.usage)
 print(model2.model_name, model2.age, model2.usage)
+print(f"{model3.model_name}, age-{model3.age}, owned by-{model3.usage}")
+print(model4.model_name, model4.age, model4.usage)
 
 
 
