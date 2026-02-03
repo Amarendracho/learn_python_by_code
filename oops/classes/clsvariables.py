954d5e8e33448dad0eca40ab18f287652350d5a8
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-03 16:08:01 -0500

    update clsvariable file

--- oops/classes/clsvariables.py
+++ oops/classes/clsvariables.py
@@ -3,15 +3,35 @@
 #                    calling class variables by obj reference and class_name.variable_name(recommended)
 
 
+
+class House:
+    walls = True
+    bedrooms= 3
+
+obj1 = House()
+obj2 = House()
+
+print(obj1.bedrooms, obj1.walls)
+print(House.bedrooms, House.walls)
+
+
+# with instance variables
 class Student:
 
     university_name = "Stanford"
+    num_students = 0
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        Student.num_students += 1
 
 student1 = Student("James", 25)
 student2 = Student("Jim", 30)
 
-print(student1.name, student1.age, Student.university_name)
+# change the class variable value
+Student.university_name = "Harvard"
+
+# calling with object name and class name
+print(student1.name, student1.age, student1.university_name, "or" , Student.university_name)
+print(Student.num_students)
