7711b32c096b6363f57add448ab61664c1eeb7f6
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-04 20:39:21 -0500

    add multilevelinheritance.py file

---
+++ oops/inheritance/multilevelinheritance.py
@@ -0,0 +1,28 @@
+# In a multilevel class hierarchy, super() ensures that all parent constructors are executed in the correct order,
+# avoiding duplicate code and properly initializing all inherited attributes.
+
+
+# Python uses Method Resolution Order (MRO) to determine the sequence in which parent classes are searched.
+# When super() is used.
+
+class Phone:
+    def __init__(self, model):
+        print("phone model : ", model)
+
+class Iphone(Phone):
+    def __init__(self, model):
+        print(model, "Model Not present in Iphone")
+        super().__init__(model)
+
+class Samsung(Iphone):
+    def __init__(self, model):
+        print(model, "Model Not present in Samsung")
+        super().__init__(model)
+
+class Nokia(Samsung):
+    def __init__(self, model):
+        print("Nokia Model : ", model)
+        super().__init__(model)
+
+phone = Nokia("Lumia 630")
+
