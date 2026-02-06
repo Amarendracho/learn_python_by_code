141d864e500ef567f69f54bfedbf62082bb07ba0
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-06 13:28:48 -0500

    update readme inbuild create operatorpoly files

--- oops/polymorphism/inbuildpoly.py
+++ oops/polymorphism/inbuildpoly.py
@@ -1,9 +1,11 @@
 # Few In-build functions in python using polymorphism
 
-print(max(2,14))
+print(max(2,14)) # 14
 
-print(len("Amar"))
+print(len("Amar")) # 4
 
-print(len([5,6,7,8,9]))
+print(len([5,6,7,8,9])) # 5
 
-print(len({"name" : "John"}))
+print(len({"name" : "John"})) # 1
+
+print(max("a", "z", "m")) # z
---
+++ oops/polymorphism/operatorpoly.py
@@ -0,0 +1,5 @@
+# Same operator, different meaning.
+
+print(15 + 10)
+print("Python " + "AI-engineering")
+print([1,3] + [4,6])
