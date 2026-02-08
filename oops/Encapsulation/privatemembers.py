5ca6e96a7639ef20305d0835b3589e8e7617104d
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-08 17:36:24 -0500

    update privatemembers.py

--- oops/Encapsulation/privatemembers.py
+++ oops/Encapsulation/privatemembers.py
@@ -1,2 +1,16 @@
 # private members and methods cannot accessed outside the class
-# define using double underscore
+# define using double underscore (self.__name/ def __details())
+
+
+class Employee:
+    def __init__(self,name, salary):
+        self.name = name
+        self.__salary = salary
+
+    def show_salary(self):
+        print("salary :" , self.__salary)
+
+emp = Employee("Mark", 150000)
+print(emp.name)
+print(emp.__salary) # # Error: Not accessible directly
+emp.show_salary() # right way to use private
