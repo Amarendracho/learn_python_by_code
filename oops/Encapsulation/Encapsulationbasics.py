92b72b681fdf5f9fa618640247cd56c1c47a5d83
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-08 20:16:29 -0500

    update protectmembers.py

--- oops/Encapsulation/Encapsulationbasics.py
+++ oops/Encapsulation/Encapsulationbasics.py
@@ -1,6 +1,8 @@
 # Encapsulation means hiding internal details of a class and only exposing what’s necessary.
 # It helps to protect important data from being changed directly and keeps the code secure and organized.
 
+# we can achieve encapsulation in 3 ways access modifiers, getters & setters , @property
+
 
 class Employee:
 
