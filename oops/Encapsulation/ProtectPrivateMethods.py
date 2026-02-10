119cc559c977b4199d38b66b28ed30af8c82e5a7
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-10 13:32:09 -0500

    upadte readme,ProtectPrivateMethods.py

--- oops/Encapsulation/ProtectPrivateMethods.py
+++ oops/Encapsulation/ProtectPrivateMethods.py
@@ -6,22 +6,25 @@ class BankAccount:
     def __init__(self):
         self.balance = 1000
 
+    # Protected method
     def _show_balance(self):
-        print(f"Balance: ₹{self.balance}")  # Protected method
+        print(f"Inital Balance: ${self.balance}")
 
+    # Private method
     def __update_balance(self, amount):
-        self.balance += amount  # Private method
+        self.balance += amount
+        print(f"Update Balance: ${self.balance}")
 
     def deposit(self, amount):
         if amount > 0:
-            self.__update_balance(amount)  # Accessing private method internally
             self._show_balance()  # Accessing protected method
+            self.__update_balance(amount)  # Accessing private method internally
         else:
             print("Invalid deposit amount!")
 
 
 account = BankAccount()
-account._show_balance()  # Works, but should be treated as internal
+#account._show_balance()  # Works, but should be treated as internal
 # account.__update_balance(500)  # Error: private method
 account.deposit(500)  # Uses both methods internally
 
