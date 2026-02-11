b4d2df2f9f3b2a80d194d34fad9124fe92c6c3a9
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-11 12:37:53 -0500

    update innerclass.py

--- oops/classes/innerclass.py
+++ oops/classes/innerclass.py
@@ -1,4 +1,21 @@
 """In Python: An inner class is a class defined inside a class.
                 They are just normal classes
                 Defined inside another class
-                Mainly used for logical grouping"""
+                Mainly used for logical grouping
+            Inner class objects depending on outer class, We can create inner class obj with help of outer class"""
+
+
+# Example University - Department
+
+class University:
+    def __init__(self,universityName):
+        self.universityName = universityName
+
+    class Department:
+        def __init__(self, deptName, staff):
+            self.deptName = deptName
+            self.staff = staff
+
+        def department_info(self):
+            print(f"Department info:{self.deptName}, No of Staff: {self.staff}")
+
