dcefa87d9bcc56ce8dcd5d41a85a06f30403addf
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-11 12:32:11 -0500

    update innerclass.py

--- oops/classes/innerclass.py
+++ oops/classes/innerclass.py
@@ -1,1 +1,4 @@
-# An inner class is a class defined inside a class.
+"""In Python: An inner class is a class defined inside a class.
+                They are just normal classes
+                Defined inside another class
+                Mainly used for logical grouping"""
