41d7047e6cc407d33ed3bc5d5ec5cdd7e68551e3
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-12 14:41:38 -0500

    update basics.py file

--- oops/constructors/basics.py
+++ oops/constructors/basics.py
@@ -9,11 +9,23 @@
 #constructor with instance variables
 class Table:
 
-    def __init__(self):
-        self.height = 10
-        self.width = 20
+    # 1 approach default values initialization inside a constructor
+    # def __init__(self):
+    #     self.height = 10
+    #     self.width = 20
+    #
+    # def default_measurements(self):
+    #     return f"{self.height}cm {self.width}cm"
 
+    # 2 approach default values initialization when constructor init
+    # def __init__(self, height = 5, width = 10):
+    #     self.height = height
+    #     self.width = width
 
 table = Table()
-print(table.height)
-print(table.width)
+# print(table.height)
+# print(table.width)
+# print(table.default_measurements())
+
+# print(table.height)
+# print(table.width)
