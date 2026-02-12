a03ed44ad54e97165f3edaacb8351757cfcbc6a6
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-12 14:28:56 -0500

    update basics.py file

--- oops/constructors/basics.py
+++ oops/constructors/basics.py
@@ -1,1 +1,7 @@
-#
+# Constructor in python special method. it automatically calls when the object is created.
+# constructor define =  __init__(self)
+# A constructor can contain [instance variables , default variables]
+# Instance variables used for object reference and object variable implementation.
+# Default variables calls with default values while we are not declare any values to instance variables
+
+#constructor with instance variables
