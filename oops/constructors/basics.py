e0cc3513fb808081aef01a17ce0a0c50bd011a90
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-12 14:34:47 -0500

    update basics.py file

--- oops/constructors/basics.py
+++ oops/constructors/basics.py
@@ -3,11 +3,17 @@
 # A constructor can contain [instance variables , default variables]
 # Instance variables used for object reference and object variable implementation.
 # Default variables calls with default values while we are not declare any values to instance variables
+# python supports 1 constructor inside a class. python not supports constructor overloading like java.
+# Because Python does not support method overloading by signature.
 
 #constructor with instance variables
 class Table:
 
     def __init__(self):
-        pass
+        self.height = 10
+        self.width = 20
+
 
 table = Table()
+print(table.height)
+print(table.width)
