d5eeac89a26b977027811d0c2ba37b4cf8068543
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-14 16:22:51 -0500

    update basics.py

--- exception_handling/basics.py
+++ exception_handling/basics.py
@@ -2,12 +2,21 @@
         without crashing .
         Instead of terminating abruptly, Python lets you detect the problem, respond to it, and continue execution when possible.
 """
-class Exception:
 
-    try:
-        a = 10
-        b = 2
-        print(a/b)
 
-    except ZeroDivisionError:
-        print("Don't do it")
+# Exception example : ZeroDivisionError: division by zero
+a = 10
+b = 0
+print(a / b)
+
+
+
+# class Exception:
+#
+#     try:
+#         a = 10
+#         b = 2
+#         print(a/b)
+#
+#     except ZeroDivisionError:
+#         print("Don't do it")
