499ac88d9b28376f4b9dd4ed95116070b792cc72
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-14 16:21:41 -0500

    update basics.py

--- exception_handling/basics.py
+++ exception_handling/basics.py
@@ -1,3 +1,7 @@
+"""Python Exception Handling allows a program to handle unexpected events (like invalid input or missing files)
+        without crashing .
+        Instead of terminating abruptly, Python lets you detect the problem, respond to it, and continue execution when possible.
+"""
 class Exception:
 
     try:
@@ -7,8 +11,3 @@ class Exception:
 
     except ZeroDivisionError:
         print("Don't do it")
-
-
-
-
-# ZeroDivisionError: division by zero
