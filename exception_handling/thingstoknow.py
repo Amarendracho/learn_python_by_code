9e8074540ac16297804889778d866de2db7b65db
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-15 17:31:36 -0500

    update thingstoknow.py file

--- exception_handling/thingstoknow.py
+++ exception_handling/thingstoknow.py
@@ -1,9 +1,12 @@
 # exception is an event that interrupt the flow of the program.
 #   when the exception happen the flow stop there and throw the exception.
-#   [example: ZeroDivisionError, NameError, TypeError]
+#   [example: ZeroDivisionError, NameError, TypeError, ValueError]
 
 
 # 1 / 0 [ZeroDivisionError]
 # 10 / name [NameError]
 #20 + "coffee" [TypeError]
-int("something") [ValueError]
+#int("something") [ValueError]
+
+name = input("Enter a number: ")
+print(name)
