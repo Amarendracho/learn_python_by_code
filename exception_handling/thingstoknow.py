374c94e96313aa831a4b97de6e0082f7bd8f6458
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-15 17:26:53 -0500

    update thingstoknow.py file

--- exception_handling/thingstoknow.py
+++ exception_handling/thingstoknow.py
@@ -1,3 +1,6 @@
 # exception is an event that interrupt the flow of the program.
 #   when the exception happen the flow stop there and throw the exception.
-#   [example ]
+#   [example: ZeroDivisionError]
+
+
+# 1 / 0 [ZeroDivisionError]
