07e8afcc9c0724e0ad9dc1637641eca1abcd9375
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-15 17:33:40 -0500

    update thingstoknow.py file

--- exception_handling/thingstoknow.py
+++ exception_handling/thingstoknow.py
@@ -2,11 +2,12 @@
 #   when the exception happen the flow stop there and throw the exception.
 #   [example: ZeroDivisionError, NameError, TypeError, ValueError]
 
+# To Handle those errors we use [try , except, finally blocks]
+
 
 # 1 / 0 [ZeroDivisionError]
 # 10 / name [NameError]
 #20 + "coffee" [TypeError]
 #int("something") [ValueError]
 
-name = input("Enter a number: ")
-print(name)
+
