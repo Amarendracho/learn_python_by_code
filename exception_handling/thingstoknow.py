227a12a0a0403f5c5d8cc19b0bb31ac31c16439a
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-15 23:06:17 -0500

    update thingstoknow.py file

--- exception_handling/thingstoknow.py
+++ exception_handling/thingstoknow.py
@@ -83,18 +83,18 @@
 
 
 # Raise an Exception
-#We raise an exception in Python using the raise keyword followed by an instance
+# We raise an exception in Python using the raise keyword followed by an instance
 # of the exception class that we want to trigger.
 
-# def voter_check(age):
-#     if age > 0:
-#         raise ValueError("Age cannot be negative")
-#     print(f"Age set to {age}")
-#
-#     try:
-#         voter_check(-3)
-#     except ValueError as e:
-#         print(e)
+def voter_check(age):
+    if age > 0:
+        raise ValueError("Age cannot be negative")
+    print(f"Age set to {age}")
+
+    try:
+        voter_check(-3)
+    except ValueError as e:
+        print(e)
 
 #Custom Exceptions
 class AgeError(Exception):
