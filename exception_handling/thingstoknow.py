7595b3f380b6dc23b86d8281e59409b96c89c67b
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-15 22:39:36 -0500

    update thingstoknow.py file

--- exception_handling/thingstoknow.py
+++ exception_handling/thingstoknow.py
@@ -1,6 +1,6 @@
 # exception is an event that interrupt the flow of the program.
 #   when the exception happen the flow stop there and throw the exception.
-#   [example: ZeroDivisionError, NameError, TypeError, ValueError]
+#   [example: ZeroDivisionError, NameError, TypeError, ValueError] - pre-defined classes
 
 # To Handle those errors we use [try , except, finally blocks]
         # use you think you block may throw an exception use [try:] block
@@ -10,6 +10,7 @@
         # We can use multiple [except blocks] for multiple exceptions.
         # If you think which exception may occur use [Exception:] block it will throw an exception not specific(Not recommend)
 
+# ErrorNames are pre-defined-classes
 
 # 1 / 0 [ZeroDivisionError]
 # 10 / name [NameError]
@@ -36,9 +37,10 @@
 # finally:
 #     print("I can execute any way! I don't care about try, except block.")
 
-try:
-    something = input("anything ?")
-    print(something / 0)
-except TypeError:
-    print("unsupported types str can;t divide by zero")
+# TypeError
+# try:
+#     something = input("anything ?")
+#     print(something / 0)
+# except TypeError:
+#     print("unsupported types str can;t divide by zero")
 
