0c6068115af2a3629311bf87e499f441cf0f0e23
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-16 20:28:23 -0500

    update /ProductionExceptionHandle.py

--- exception_handling/ProductionExceptionHandle.py
+++ exception_handling/ProductionExceptionHandle.py
@@ -1,5 +1,11 @@
 # In production, you usually use with open(...) instead of manual finally:
 
+# ✅ Don’t just print errors
+# ✅ Log them
+# ✅ Catch only what you can handle
+# ✅ Let unexpected bugs fail loudly (or be handled centrally)
+
+
 with open("data.txt") as f:
     data = f.read()
 
