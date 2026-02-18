9bb9e351f36986bf31f5e0cde647e002a7df8046
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-18 15:54:19 -0500

    update Test.py

--- exception_handling/Test.py
+++ exception_handling/Test.py
@@ -1,11 +1,18 @@
 
-try:
-    number = int(input("Enter a number : "))
-    print(10 / number)
-except ZeroDivisionError:
-    print("Number can't be divided by Zero")
-except ValueError:
-    print("Number can't be divided by String ")
-finally:
-    print("I am executing anyway... ")
+# try:
+#     number = int(input("Enter a number : "))
+#     print(10 / number)
+# except ZeroDivisionError:
+#     print("Number can't be divided by Zero")
+# except ValueError:
+#     print("Number can't be divided by String ")
+# finally:
+#     print("I am executing anyway... ")
 
+
+try:
+    for i in range(2):
+        print(i)
+
+except (IndexError, ZeroDivisionError) :
+    print("Something happen")
