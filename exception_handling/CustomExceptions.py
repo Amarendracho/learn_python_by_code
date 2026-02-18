3a9e510d4af738d692bbff0b92626085d600d2e6
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-18 17:14:20 -0500

    update CustomExceptions.py

--- exception_handling/CustomExceptions.py
+++ exception_handling/CustomExceptions.py
@@ -1,16 +1,17 @@
 # use raise keyword to give a custom message error
 
-def age_Check():
-    age = int(input("Enter your age :"))
-    if age < 0:
-        raise ValueError("Age cannot be -ve")
-    elif age <= 18:
-        print("Still Minor")
-    elif age >= 18:
-        print(f"Your age {age}\nSafe Drive")
+# def age_Check():
+#     age = int(input("Enter your age :"))
+#     if age < 0:
+#         raise ValueError("Age cannot be -ve")
+#     elif age <= 18:
+#         print("Still Minor")
+#     elif age >= 18:
+#         print(f"Your age {age}\nSafe Drive")
+#
+#
+# try:
+#     age_Check()
+# except ValueError as e:
+#     print(e)
 
-
-try:
-    age_Check()
-except ValueError as e:
-    print(e)
