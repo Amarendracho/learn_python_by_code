0e77687b0fdde6bf64fb4f84983e2d1ac80b4947
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-19 15:43:19 -0500

    update CustomExceptions.py

--- exception_handling/CustomExceptions.py
+++ exception_handling/CustomExceptions.py
@@ -2,22 +2,22 @@
 # Exception class inherit in-build python exception class called(Exception)
 
 # age check
-class AgeError(Exception):
-    pass
-def setAge(age):
-    if age < 0:
-        raise AgeError("Age cannot be -ve")
-    print(f"age set to {age}")
-
-try:
-    setAge(-1)
-except AgeError as e:
-    print(e)
-
-# Payment limit example
-class PaymentFailedError(Exception):
-    pass
-
-def charge(card, amount):
-    if amount > 10000:
-        raise PaymentFailedError("limit exceeded")
+# class AgeError(Exception):
+#     pass
+# def setAge(age):
+#     if age < 0:
+#         raise AgeError("Age cannot be -ve")
+#     print(f"age set to {age}")
+#
+# try:
+#     setAge(-1)
+# except AgeError as e:
+#     print(e)
+#
+# # Payment limit example
+# class PaymentFailedError(Exception):
+#     pass
+#
+# def charge(card, amount):
+#     if amount > 10000:
+#         raise PaymentFailedError("limit exceeded")
