d4beb8cd2c1ca42290d080b770062a6f50effe03
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-19 17:53:04 -0500

    update in-build exceptions

--- exception_handling/BuildInExceptions.py
+++ exception_handling/BuildInExceptions.py
@@ -15,3 +15,11 @@ try:
     raise BaseException("This is BaseException")
 except BaseException as e:
     print(e)
+
+#Exception  Class
+# The Exception class is the base for all non-exit exceptions.
+# You will often catch Exception in general error-handling code when you are not targeting a specific error type.
+try:
+    raise Exception("This is a generic exception")
+except Exception as e:
+    print(e)
