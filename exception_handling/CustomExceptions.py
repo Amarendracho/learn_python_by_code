3c55624c8478d891c07b11e67759f3afef2a1d36
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-19 15:50:46 -0500

    update CustomExceptions.py

--- exception_handling/CustomExceptions.py
+++ exception_handling/CustomExceptions.py
@@ -3,32 +3,37 @@
 
 
 # age check
-# class AgeError(Exception):
-#     pass
-# def setAge(age):
-#     if age < 0:
-#         raise AgeError("Age cannot be -ve")
-#     print(f"age set to {age}")
-#
-# try:
-#     setAge(-1)
-# except AgeError as e:
-#     print(e)
-#
-# # Payment limit example
-# class PaymentFailedError(Exception):
-#     pass
-#
-# def charge(card, amount):
-#     if amount > 10000:
-#         raise PaymentFailedError("limit exceeded")
-
-
-import logging
-logger = logging.getLogger(__name__)
+class AgeError(Exception):
+    pass
+def setAge(age):
+    if age < 0:
+        raise AgeError("Age cannot be -ve")
+    print(f"age set to {age}")
 
 try:
-    1/0
-except ZeroDivisionError:
-    logger.exception("Division failed")
+    setAge(-1)
+except AgeError as e:
+    print(e)
+
+# Payment limit example
+class PaymentFailedError(Exception):
+    pass
+
+def charge(card, amount):
+    if amount > 10000:
+        raise PaymentFailedError("limit exceeded")
+    return amount
+
+try:
+    print(charge("CITY",12000))
+except PaymentFailedError as e:
+    print(e)
+
+# import logging
+# logger = logging.getLogger(__name__)
+#
+# try:
+#     1/0
+# except ZeroDivisionError:
+#     logger.exception("Division failed")
 #logger.exception() is great in production because it includes the full traceback.
