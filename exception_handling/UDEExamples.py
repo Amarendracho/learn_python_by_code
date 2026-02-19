3a8616e2c97a182fc408ead73887e0f68b93f267
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-19 18:29:31 -0500

    update README

--- exception_handling/UDEExamples.py
+++ exception_handling/UDEExamples.py
@@ -1,25 +1,25 @@
-# # Step 1: Define a custom exception class
-# class InvalidAgeError(Exception):
-#     def __init__(self, age, msg="Age must be between 0 and 120"):
-#         self.age = age
-#         self.msg = msg
-#
-#
-#     def __str__(self):
-#         return f'{self.age} -> {self.msg}'
-#
-# # Step 2: Use the custom exception in your code
-# def set_age(age):
-#     if age < 0 or age > 120:
-#         raise InvalidAgeError(age)
-#     else:
-#         print(f"Age set to: {age}")
-#
-# # Step 3: Handling the custom exception
-# try:
-#     set_age(150)  # This will raise the custom exception
-# except InvalidAgeError as e:
-#     print(e)
+# Step 1: Define a custom exception class
+class InvalidAgeError(Exception):
+    def __init__(self, age, msg="Age must be between 0 and 120"):
+        self.age = age
+        self.msg = msg
+
+
+    def __str__(self):
+        return f'{self.age} -> {self.msg}'
+
+# Step 2: Use the custom exception in your code
+def set_age(age):
+    if age < 0 or age > 120:
+        raise InvalidAgeError(age)
+    else:
+        print(f"Age set to: {age}")
+
+# Step 3: Handling the custom exception
+try:
+    set_age(150)  # This will raise the custom exception
+except InvalidAgeError as e:
+    print(e)
 
 
 class InvalidAgeError(Exception):
@@ -32,13 +32,11 @@ class InvalidAgeError(Exception):
     def __str__(self):
         return f"[Error Code {self.error_code}] {self.age} -> {self.msg}"
 
-
 def set_age(age):
     if age < 0 or age > 120:
         raise InvalidAgeError(age)
     else:
         print(f"Age set to: {age}")
-
 
 try:
     set_age(150)  # This will raise the custom exception
