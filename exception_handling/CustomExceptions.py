dcc6e7a6dc2cac3b6e6641b8133b40c1ccc756b2
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-19 15:25:48 -0500

    update CustomExceptions.py

--- exception_handling/CustomExceptions.py
+++ exception_handling/CustomExceptions.py
@@ -1,8 +1,8 @@
 # Custom exception used in production. Create separate classes for exception.
-
+# Exception class inherit in-build python exception class called(Exception)
 
 # age check
-class AgeError:
+class AgeError(Exception):
     pass
 
 def
