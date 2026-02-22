cf72b6d40fc45a07a54a88b9de3b041129ba7844
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-22 02:21:45 -0500

    update FIleBasics.py

--- files/FilesBasics.py
+++ files/FilesBasics.py
@@ -1,6 +1,7 @@
 # File handling is teh process of performing operations on files.
 
-# First open a file to open use open() function
+# First open a file to open use open() function.
+#                               open() - 2 parameters(filename, mode)
 
 file = open("readme.txt")
 # read mode
