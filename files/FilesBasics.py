c546f415d2a55073075e734c79a457fe8ee0cf3b
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-22 02:43:14 -0500

    update FIleBasics.py

--- files/FilesBasics.py
+++ files/FilesBasics.py
@@ -4,15 +4,30 @@
 #                               open() - 2 parameters(filename, mode)
 #                               filename("samedirectory_location_path" / "fulldirectory_path")
 
+# open File -
+# file = open("readme.txt", "r")
+# f = open("/Users/amarendrak/PycharmProjects/python_codepractice/newfile.txt", "r")
+
+# read File - file not there FileNotFound Exception
+# file = open("readme.txt", "r")
+# f = open("/Users/amarendrak/PycharmProjects/python_codepractice/newfile.txt", "r")
+
+# print(file.read())
+# print(f.read())
+
+# Create file
 file = open("readme.txt", "r")
-print(file.read())
 f = open("/Users/amarendrak/PycharmProjects/python_codepractice/newfile.txt", "r")
+
+# file = open("readme.txt", "r")
+# print(file.read())
+# f = open("/Users/amarendrak/PycharmProjects/python_codepractice/newfile.txt", "r")
 
 #print(f.read())
 
 # read mode
 
 
-file1 = open("newfile.txt", "w")
-
-file2 = open("newfile1.txt", "a")
+# file1 = open("newfile.txt", "w")
+#
+# file2 = open("newfile1.txt", "a")
