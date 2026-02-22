489956a97c9bc32764c1e96d8c5a7bf1a8ec1a90
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-22 03:02:36 -0500

    update FIleBasics.py

--- files/FilesBasics.py
+++ files/FilesBasics.py
@@ -5,38 +5,36 @@
 #                               filename("samedirectory_location_path" / "fulldirectory_path")
 
 # open File -
-# file = open("readme.txt", "r")
-# f = open("/Users/amarendrak/PycharmProjects/python_codepractice/newfile.txt", "r")
+file = open("readme.txt", "r")
+f = open("/Users/amarendrak/PycharmProjects/python_codepractice/newfile.txt", "r")
 
 # read File - r mode -  file not there FileNotFound Exception
-# file = open("readme.txt", "r")
-# f = open("/Users/amarendrak/PycharmProjects/python_codepractice/newfile.txt", "r")
+file = open("readme.txt", "r")
+f = open("/Users/amarendrak/PycharmProjects/python_codepractice/newfile.txt", "r")
 
-# print(file.read())
-# print(f.read())
+print(file.read())
+print(f.read())
 
 # Create file - x mode
-# file = open("readme.txt", "x")
-# f = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/newfile.txt", "x")
+file = open("readme.txt", "x")
+f = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/newfile.txt", "x")
 
 # write mode - w mode and read mode
-# file = open("readme.txt", "w")
-# file.write("This is readme file! \n you can read me")
-# file.close()
-#
-# file = open("readme.txt", "r")
-# print(file.read())
-#
-# #f = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/newfile.txt", "x")
+file = open("readme.txt", "w")
+file.write("This is readme file! \n you can read me")
+file.close()
+
+file = open("readme.txt", "r")
+print(file.read())
 
 
-# # Checking File Properties
-# file = open("newfile.txt", "r")
-# print("Checking :", file.name)
-# print("Mode :", file.mode)
-# print("Is Closing? :", file.closed)
-#
-# file.close()
-# print("Is Closing? :", file.closed)
+# Checking File Properties
+file = open("newfile.txt", "r")
+print("Checking :", file.name)
+print("Mode :", file.mode)
+print("Is Closing? :", file.closed)
+
+file.close()
+print("Is Closing? :", file.closed)
 
 
