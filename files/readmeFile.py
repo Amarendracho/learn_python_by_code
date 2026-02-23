6ed6929679072e85ddc888df81169031fd746b8f
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-22 22:45:28 -0500

    readmeFile

--- files/readmeFile.py
+++ files/readmeFile.py
@@ -1,5 +1,7 @@
 
 
+#open the file in read mode. By default, it processes as read mode
+#file = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/car.txt")
 file = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/car.txt", "r")
 
 content = file.read()
