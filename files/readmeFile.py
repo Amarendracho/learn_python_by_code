30fdb7784235ace7e3f0989e1033ad3095c28b48
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-22 22:46:34 -0500

    readmeFile

--- files/readmeFile.py
+++ files/readmeFile.py
@@ -4,5 +4,6 @@
 #file = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/car.txt")
 file = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/car.txt", "r")
 
+#read the file and saved in content variable
 content = file.read()
 print(content)
