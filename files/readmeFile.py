23e9a32a55e3026410b80e5a9dd3fd894b42b50f
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-23 22:33:41 -0500

    update readmeFile

--- files/readmeFile.py
+++ files/readmeFile.py
@@ -2,24 +2,24 @@
 
 #open the file in read mode. By default, it processes as read mode
 #file = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/car.txt")
-file = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/car.txt", "r")
-
-#read the file and saved in content variable
-content = file.read()
-print(content)
-
-# Once you open a file you mush close it to.
-file.close()
-
-
-
-# creating file
-f = open("NewFile.txt", "x")
-
-#Using with statement - no need to close file manually.
-with open("NewFile.txt", "r") as file1:
-    content1 = file1.read()
-    print(content1)
+# file = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/car.txt", "r")
+#
+# #read the file and saved in content variable
+# content = file.read()
+# print(content)
+#
+# # Once you open a file you mush close it to.
+# file.close()
+#
+#
+#
+# # creating file
+# f = open("NewFile.txt", "x")
+#
+# #Using with statement - no need to close file manually.
+# with open("NewFile.txt", "r") as file1:
+#     content1 = file1.read()
+#     print(content1)
 
 # Reading a File Line by Line - 2 approaches
 # 1. Using a Loop to Read Line by Line
