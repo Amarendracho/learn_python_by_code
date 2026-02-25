10d89df64575fb501eb9763eeddf4727473dbd70
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-24 22:53:00 -0500

    update readmeFile

--- files/readmeFile.py
+++ files/readmeFile.py
@@ -1,76 +1,75 @@
 
 
 #open the file in read mode. By default, it processes as read mode
-#file = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/car.txt")
-# file = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/car.txt", "r")
-#
-# #read the file and saved in content variable
-# content = file.read()
-# print(content)
-#
-# # Once you open a file you mush close it to.
-# file.close()
-#
-#
-#
-# # creating file
-# f = open("NewFile.txt", "x")
-#
-# #Using with statement - no need to close file manually.
-# with open("NewFile.txt", "r") as file1:
-#     content1 = file1.read()
-#     print(content1)
+file = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/car.txt")
+file = open("/Users/amarendrak/PycharmProjects/python_codepractice/files/car.txt", "r")
 
-# # Reading a File Line by Line - 2 approaches
-# # 1. Using a Loop to Read Line by Line
-# file2 =  open("car.txt", "r")
-# for line in file2:
-#     print(line.strip()) # .strip() to remove newline characters
-# file2.close()
+#read the file and saved in content variable
+content = file.read()
+print(content)
 
-# # close automatically
-# with open("car.txt", "r") as file3:
-#     for lines in file3:
-#         print(lines.strip())
+# Once you open a file you mush close it to.
+file.close()
 
-# # 2. Using readline()
-# file4 = open("NewFile.txt", "r")
-# line = file4.readline()
-# while line:
-#     print(line.strip())
-#     line = file4.readline() # We can keep calling it inside a loop until no lines are left.
-# file4.close()
 
-## Reading Binary Files
-# with open("NewFile.bin", "rb") as file:
-#     content = file.read()
-#     print(content)
+# creating file
+f = open("NewFile.txt", "x")
 
-# #Reading Specific Parts of a File
-# file = open("NewFile.txt", "r")
-# content = file.read(10)
-# print(content)
-# file.close()
+#Using with statement - no need to close file manually.
+with open("NewFile.txt", "r") as file1:
+    content1 = file1.read()
+    print(content1)
+
+# Reading a File Line by Line - 2 approaches
+# 1. Using a Loop to Read Line by Line
+file2 =  open("car.txt", "r")
+for line in file2:
+    print(line.strip()) # .strip() to remove newline characters
+file2.close()
+
+# close automatically
+with open("car.txt", "r") as file3:
+    for lines in file3:
+        print(lines.strip())
+
+# 2. Using readline()
+file4 = open("NewFile.txt", "r")
+line = file4.readline()
+while line:
+    print(line.strip())
+    line = file4.readline() # We can keep calling it inside a loop until no lines are left.
+file4.close()
+
+# Reading Binary Files
+with open("NewFile.bin", "rb") as file:
+    content = file.read()
+    print(content)
+
+#Reading Specific Parts of a File
+file = open("NewFile.txt", "r")
+content = file.read(10)
+print(content)
+file.close()
 
 # using with
-# with open("NewFile.txt", "r") as file:
-#     content = file.read(18)
-#     print(content)
+with open("NewFile.txt", "r") as file:
+    content = file.read(18)
+    print(content)
 
-# #Reading CSV Files
-# import csv
-# import io
-#
-# # Create a CSV sample in memory
-# csv_data = """Year,Industry,Value
-# 2014,Manufacturing,769400
-# 2014,Manufacturing,48000
-# 2014,Manufacturing,12
-# """
-# csvfile = io.StringIO(csv_data)
-# csvreader = csv.reader(csvfile)
-# for row in csvreader:
-#     print(row)
+#Reading CSV Files
+import csv
+import io
+
+# Create a CSV sample in memory
+csv_data = """Year,Industry,Value
+2014,Manufacturing,769400
+2014,Manufacturing,48000
+2014,Manufacturing,12
+"""
+csvfile = io.StringIO(csv_data)
+csvreader = csv.reader(csvfile)
+for row in csvreader:
+    print(row)
 
 # Reading JSON Files
 import json
