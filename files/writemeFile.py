8ad7a21c62fc9f0a20a3be6877066f2c08dde24e
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-25 22:29:10 -0500

    add writeFile

--- files/writemeFile.py
+++ files/writemeFile.py
@@ -6,10 +6,11 @@
 #
 #     print(file.read()) # we can't read the file io.UnsupportedOperation: not readable
 
-with open("NewFile.txt", "w", encoding= "utf-8") as file:
-    file.write("This is Example for override the file. \n")
-    file.write("If the file already exists it delete and create another file as same name")
-
-with open("NewFile.txt", "r", encoding="utf-8") as file:
-    content = file.read()
-    print(content)
+# # Right way
+# with open("NewFile.txt", "w", encoding= "utf-8") as file:
+#     file.write("This is Example for override the file. \n")
+#     file.write("If the file already exists it delete and create another file as same name")
+#
+# with open("NewFile.txt", "r", encoding="utf-8") as file:
+#     content = file.read()
+#     print(content)
