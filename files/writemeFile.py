cd503a946221bb680c3b44f1beb8afca3b7a6d32
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-25 22:41:40 -0500

    update writemeFile

--- files/writemeFile.py
+++ files/writemeFile.py
@@ -29,3 +29,10 @@
 # except FileExistsError:
 #     print("NewFile.txt already exists, exclusive creation aborted.")
 
+#Writing multiple lines
+lines = ["First line\n", "Second line\n", "Third line\n"]
+with open("file1.txt", "w", encoding="utf-8") as file:
+    file.writelines(lines)
+
+with open("file1.txt", "r", encoding="utf-8") as file:
+    print(file.read())
