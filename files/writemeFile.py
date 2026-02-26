3822f7d9cec7499d392b0a429f830cd1042e69e9
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-25 22:34:30 -0500

    update files

--- files/writemeFile.py
+++ files/writemeFile.py
@@ -14,3 +14,10 @@
 # with open("NewFile.txt", "r", encoding="utf-8") as file:
 #     content = file.read()
 #     print(content)
+
+# Append to an existing file
+with open("NewFile.txt", "a", encoding="utf-8") as file:
+    file.write("\nThis is append method, whatever you add appears at the end!")
+
+with open("NewFile.txt", "r", encoding="utf-8") as file:
+    print(file.read())
