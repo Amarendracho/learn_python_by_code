0b239e86871672aa8fa5957a0c92013beb44438c
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-28 19:13:58 -0500

    update inputOutputOperations.py

--- inputOutputOperations.py
+++ inputOutputOperations.py
@@ -1,3 +1,8 @@
 # input and output operations is fundamental to Python programming.
 #       With the print() function, we can display output in various formats,
 #       while the input() function enables interaction with users by gathering input during program execution.
+
+
+# Taking input in Python - input() function
+# input() method return a string what to convert concat with other datatypes(int, float,...)
+input("")
