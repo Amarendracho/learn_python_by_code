92d894abb6f88b679fcdb54c1685f3be38bb9cd0
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-28 19:21:07 -0500

    update inputOutputOperations.py

--- inputOutputOperations.py
+++ inputOutputOperations.py
@@ -7,8 +7,11 @@
 # input() method return a string what to convert concat with other datatypes(int, float,...)
 # print() prints the output
 
-name = input("Enter your favourite singer: ")
-print(name,"is My favourite singer 🎶")
+# name = input("Enter your favourite singer: ")
+# print(name,"is My favourite singer 🎶")
 
 # The code prompts the user to input their name, stores it in the variable "name"
 # and then prints a greeting message addressing the user by their entered name.
+
+# Printing Output using print() in Python
+print("Something")
