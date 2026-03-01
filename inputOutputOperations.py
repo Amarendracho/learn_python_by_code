607ee28deb0c36b65bebfedd17da9e2632ce8320
Amarendra <Kadambala.amarendra@gmail.com>
2026-02-28 19:12:07 -0500

    update inputOutputOperations.py

---
+++ inputOutputOperations.py
@@ -0,0 +1,3 @@
+# input and output operations is fundamental to Python programming.
+#       With the print() function, we can display output in various formats,
+#       while the input() function enables interaction with users by gathering input during program execution.
