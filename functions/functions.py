555ffe8828003db4e031e2eddbf5f64a7392e7ff
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-03 20:23:59 -0500

    update functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -40,3 +40,9 @@
 # person("JUSTIN")
 
 # 2. KEYWORD ARGUMENTS - PASSED BY EXPLICITLY SPECIFYING THE PARAMETER NAMES, SO THE ORDER DOESN’T MATTER
+
+def customer(fname, lname):
+    print(f"FIRST NAME: {fname}, LAST NAME: {lname}")
+
+customer(fname="Alex", lname="Bob")
+customer(fname="Justin", lname="Bieber")
