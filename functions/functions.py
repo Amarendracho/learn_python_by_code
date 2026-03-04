5ad362587e248db8fc3a662de34da1a0ace80f1e
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-03 20:07:14 -0500

    update functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -2,4 +2,10 @@
     repeated / common code tasks reuse. instead of same code write it again, and again we function to repeat.
 """
 
-# We define a function using keyword called - def function_name
+# We define a function using keyword called - def function_name:
+
+def watching():
+    print("WATCHING A WEB SERIES... GRAY'S ANATOMY")
+
+# CALLING THE FUNCTION IS SIMPLE CALL WITH NAME OF THE FUNCTION.
+watching()
