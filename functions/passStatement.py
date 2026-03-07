4e110d4ec01e7f92f53901aef5d7d458bb34fc26
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-06 20:23:34 -0500

    add  update passStatement.py

--- functions/passStatement.py
+++ functions/passStatement.py
@@ -1,6 +1,8 @@
             # pass Statement
 
 # THE PASS STATEMENT IN PYTHON IS A PLACEHOLDER THAT DOES NOTHING WHEN EXECUTED.
+# IT IS USED TO KEEP CODE BLOCKS VALID WHERE A STATEMENT IS REQUIRED BUT NO LOGIC IS NEEDED YET.
+# Examples situations where pass is used are empty functions, classes, loops or conditional blocks.
 
-name = ""
+name = "It is used to keep code blocks valid where a statement is required but no logic is needed yet."
 print(name.upper())
