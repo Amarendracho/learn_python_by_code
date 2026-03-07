cb2f16e6813d7077ee513f9dd85689426057efde
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-06 20:32:34 -0500

    add  update passStatement.py

--- functions/passStatement.py
+++ functions/passStatement.py
@@ -4,7 +4,7 @@
 # IT IS USED TO KEEP CODE BLOCKS VALID WHERE A STATEMENT IS REQUIRED BUT NO LOGIC IS NEEDED YET.
 # EXAMPLES SITUATIONS WHERE PASS IS USED ARE EMPTY FUNCTIONS, CLASSES, LOOPS OR CONDITIONAL BLOCKS.
 
-name = "In Conditional Statements"
+name = "Do nothing when i is 3"
 print(name.upper())
 
 # IN FUNCTIONS
@@ -14,17 +14,17 @@ def hello():
 
 hello()
 
-# IN CONDITIONAL STATEMENTS
-age = 17
-if age >= 18:
-    pass
-else:
-    print("Not Eligible")
+# # IN CONDITIONAL STATEMENTS
+# age = 17
+# if age >= 18:
+#     pass
+# else:
+#     print("Not Eligible")
 
 # IN LOOPS
 
-for i in range(1, 11):
+for i in range(1, 5):
     if i == 3:
-        pass
+        pass        # DO NOTHING WHEN I IS 3
     else:
         print(i)
