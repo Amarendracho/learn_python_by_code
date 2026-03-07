ddb903dba879f7b8b2699451304cb0b8aea09359
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-06 20:16:25 -0500

    add  update passStatement.py

--- functions/passStatement.py
+++ functions/passStatement.py
@@ -1,2 +1,6 @@
-# pass Statement
+            # pass Statement
 
+# THE PASS STATEMENT IN PYTHON IS A PLACEHOLDER THAT DOES NOTHING WHEN EXECUTED.
+
+name = "The pass statement in Python is a placeholder that does nothing when executed."
+print(name.upper())
