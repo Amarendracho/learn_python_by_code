f87addc07729607eafa11f58c6f76504b182f150
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-08 20:21:08 -0400

    update Recursion.py

--- functions/Recursion.py
+++ functions/Recursion.py
@@ -5,5 +5,10 @@
 #                  A RECURSIVE FUNCTION IS JUST LIKE ANY OTHER PYTHON FUNCTION EXCEPT THAT IT CALLS ITSELF IN ITS BODY.
 
 
+"""def recursive_function(parameters):
+    if base_case_condition:
+        return base_result
+    else:
+        return recursive_function(modified_parameters)"""
 name = "A recursive function is just like any other Python function except that it calls itself in its body"
 print(name.upper())
