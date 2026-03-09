5bdf61bdf4f6e26bba54d8c9bc34bbe36a27bae1
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-08 22:18:05 -0400

    update Recursion.py

--- functions/Recursion.py
+++ functions/Recursion.py
@@ -31,13 +31,12 @@ print(name.upper())
 
 # EXAMPLE 2: FIBONACCI SEQUENCE 5 = 5 + 4 + 3 + 2 + 1 = 15
 
-def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return n + fibonacci(n - 1)
-
-
-print(fibonacci(10))
+# def fibonacci(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return n + fibonacci(n - 1)
+#
+# print(fibonacci(10))
