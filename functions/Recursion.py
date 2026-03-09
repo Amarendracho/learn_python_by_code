b682b57f5ed8ba20f3253caa1f9f6a798f310ae6
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-08 20:18:31 -0400

    update Recursion.py

--- functions/Recursion.py
+++ functions/Recursion.py
@@ -1,2 +1,6 @@
-# RECURSION -
+# RECURSION - RECURSION IS A PROGRAMMING TECHNIQUE WHERE A FUNCTION CALLS ITSELF EITHER DIRECTLY OR INDIRECTLY
+#             TO SOLVE A PROBLEM BY BREAKING IT INTO SMALLER, SIMPLER SUBPROBLEMS.
 
+
+name = "Recursion is a programming technique where a function calls itself either directly or indirectly to solve a problem by breaking it into smaller, simpler subproblems."
+print(name.upper())
