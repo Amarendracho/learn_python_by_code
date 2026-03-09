6f03d027e478fc31d72c6b56f9aea5bb1686a1c4
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-08 22:15:38 -0400

    update Recursion.py

--- functions/Recursion.py
+++ functions/Recursion.py
@@ -30,3 +30,12 @@ print(name.upper())
 # print(factorial(5))
 
 # EXAMPLE 2: FIBONACCI SEQUENCE 5 = 5 + 4 + 3 + 2 + 1 = 15
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return n + fibonacci(n - 1)
+
