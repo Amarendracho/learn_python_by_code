decf14e91d3018e0e119a1b16822fb1253658697
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-10 18:48:18 -0400

    update argsandkwargs file

--- functions/argsandkwargs.py
+++ functions/argsandkwargs.py
@@ -1,4 +1,4 @@
-
+# *ARGS AND **KWARGS** - ARE USED WHEN A FUNCTION DOES NOT KNOW IN ADVANCE HOW MANY ARGUMENTS IT WILL RECEIVE.
 
 name = "*args and **kwargs are used when a function does not know in advance how many arguments it will receive."
 print(name.upper())
