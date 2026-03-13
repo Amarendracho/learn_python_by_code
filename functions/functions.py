378a90fdf473397ae871054b846696343aef8087
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-13 17:39:25 -0400

    update functions.py file

--- functions/functions.py
+++ functions/functions.py
@@ -1,5 +1,5 @@
 """ Python Functions are a block of statements that does a specific task. functions are used to
-    repeated / common code tasks reuse. instead of same code write it again, and again we use function to repeat.
+    repeated / common code tasks reuse. instead of same code write it again, and again we use functions to repeat.
 """
 
 # We define a function using keyword called - def function_name:
