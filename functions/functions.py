2516e3a8a629262187377965d435a0c091ba74ac
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-16 20:22:15 -0400

    update functions/functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -63,6 +63,8 @@ def employee(name, salary):
 employee("Derek", 455000.21)
 employee(234423.22, "Mark")
 
+def car_details(brand, cost):
+
 
 # 4. ARBITRARY ARGUMENTS - *args (KEYWORD ARGUMENTS)
 #                          **kwargs (NON-KEYWORD ARGUMENTS)
