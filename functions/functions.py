4ff89e29fad9ea1245fc4d4e2a6a00515b337ab7
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-17 19:15:44 -0400

    update functions/functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -149,11 +149,6 @@ def square(num):
 
 print(square(5))
 
-def add(a,b,c):
-    return "a"+ "b" + "c"
-
-print(add(4,5,6))
-
 # RECURSIVE FUNCTIONS - A RECURSIVE FUNCTION IS A FUNCTION THAT CALLS ITSELF TO SOLVE A PROBLEM
 
 def factorial(n):
