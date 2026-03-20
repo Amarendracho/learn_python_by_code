c23f39385ef76588bc7ae96f28d76b5f97bee165
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-19 20:33:35 -0400

    UPDTAE InnerFunction.py

--- functions/InnerFunction.py
+++ functions/InnerFunction.py
@@ -13,5 +13,6 @@ outer("Amar")
 
 # SCOPE OF VARIABLES IN INNER FUNCTIONS
 # INNER FUNCTIONS FOLLOW PYTHON'S - LEGB RULE (LOCAL --> ENCLOSING --> GLOBAL --> BUILT-IN).
+# INNER FUNCTIONS ACCESS OUTER FUNCTION VARIABLES, BUT MODIFYING THEM REQUIRES SPECIAL KEYWORDS LIKE (nonlocal).
 name = "access outer function variables, but modifying them requires special keywords like nonlocal."
 print(name.upper())
