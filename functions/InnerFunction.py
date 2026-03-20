e7e0d2a55838a3f4664464389facc9c07462f289
Amarendra <Kadambala.amarendra@gmail.com>
2026-03-19 20:54:52 -0400

    UPDTAE InnerFunction.py

--- functions/InnerFunction.py
+++ functions/InnerFunction.py
@@ -43,10 +43,25 @@ carBrand()
 def carBrand(name):
     def carModel():
         print(name)
-    return carModel()
+    return carModel # NOT CALLING
 
-closure = carBrand("mustang")
+closure = carBrand("AUDI")
 closure()
+
+
+def outer_function(x):
+    # Outer function: takes 'x' and defines inner_function
+    def inner_function(y):
+        return x + y  # 'x' is remembered from outer_function
+    return inner_function  # Returns inner function (closure)
+
+# Create a closure with x = 10
+closure = outer_function(10)
+
+# Call the closure with different values of 'y'
+print(closure(5))
+print(closure(20))
+
 
 
 name = "Closure in inner function"
