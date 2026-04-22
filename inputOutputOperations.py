d5672fdae9b6d086a21beb70e9862a73d9ea215a
Amarendra <Kadambala.amarendra@gmail.com>
2026-04-22 17:15:43 -0400

    update inputOutputOperations.py

--- inputOutputOperations.py
+++ inputOutputOperations.py
@@ -34,7 +34,7 @@ thing1 , thing2 = input("Enter your favourite things: ?").split()
 print("My favourite thing 1: ", thing1)
 print("My favourite thing 2: ", thing2)
 
-# WHILE ENTER THE VALUES
+# WHILE ENTER THE VALUES SEPARATED
 
 a,b,c = input("Your Top Schools: ? ").split()
 print("My Top college is:",a)
