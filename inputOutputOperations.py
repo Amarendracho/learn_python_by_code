f315bcbe6f189a63fb5c58704a697dac0fc20cf6
Amarendra <Kadambala.amarendra@gmail.com>
2026-04-22 17:09:55 -0400

    update inputOutputOperations.py

--- inputOutputOperations.py
+++ inputOutputOperations.py
@@ -23,8 +23,8 @@ print("Person Details")
 name = "MarkSloan"
 age = 26
 address = "1-12 BEVERLY HILLS FLORIDA"
-print(name) # PRINTING A SINGLE VARIABLE
-print("Name:",name, ":)age:",age, "!address:",address) # PRINTING MULTIPLE VARIABLES SEPARATED BY COMMA
+print(name)
+print("Name:",name, ":)age:",age, "!address:",address) # PRINTING MULTIPLE VARIABLES SEPARATED BY COMMA.
 
 #Take Multiple Input in Python
 # We are taking multiple input from the user in a single line, splitting the values entered by the user
