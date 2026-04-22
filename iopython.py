176049220fd82cc3f1f4bf57b35bf7d13f40dbf7
Amarendra <Kadambala.amarendra@gmail.com>
2026-04-22 17:11:46 -0400

    update  iopython.py

--- iopython.py
+++ iopython.py
@@ -5,3 +5,8 @@ age = 32
 country = "Canada"
 
 print(name, age, country)
+
+
+thing1 , thing2 = input("Enter your favourite things: ?").split()
+print("My favourite thing 1: ", thing1)
+print("My favourite thing 2: ", thing2)
